f68682f4290b366b84cfacf012d35ab255408d83
hariseldon2073 <hariseldon2073@gmail.com>
2021-10-19 15:39:01 +0200

    Hello GIT BRANCH 009

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello GIT 3");
+        System.out.println("Hello GIT ***");
     }
+
+    private void doTest() {
+
+    }
+
+    /*BRANCH COMMENT*/
+    private void doTest(String name) {
+
+    }
+
+    /*BRANCH COMMENT 001 */
+    private void dummyMeth() {
+
+    }
+
 }
+
